d86ae92c56c100fe9db18f14bc1b149ee1c1ab7a
Lucas Theiss <lucasptheiss@gmail.com>
2023-08-22 21:19:08 -0300

    Update java webapp docker compose, controler, files .jar. Running

--- java/webapp/src/main/java/com/mywebapp/controller/controler.java
+++ java/webapp/src/main/java/com/mywebapp/controller/controler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class HelloController {
+public class controler {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello, world!";
