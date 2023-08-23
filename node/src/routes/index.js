4ab7d742bd2a7d067afa62a87e5826904ae3fa32
Diogo Klein <20893375+digodk@users.noreply.github.com>
2023-08-23 20:02:34 -0300

    modificando aplicação node para acesso a banco

--- node/src/routes/index.js
+++ node/src/routes/index.js
@@ -2,7 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello, World!');
+  const db = req.app.locals.db;
+  
+  db.query('CREATE TABLE IF NOT EXISTS visits (count INT)', (err) => {
+    if (err) throw err;
+
+    db.query('INSERT INTO visits (count) VALUES (1)', (err) => {
+      if (err) throw err;
+
+      db.query('SELECT COUNT(*) as visitCount FROM visits', (err, results) => {
+        if (err) throw err;
+
+        res.send(`Olá, mundo! Essa página foi visitada ${results[0].visitCount} vezes.`);
+      });
+    });
+  });
 });
 
 module.exports = router;
