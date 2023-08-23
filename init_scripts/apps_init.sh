de99029da53acc6efd9b8a1098ec81f9e20fba19
Diogo Klein <20893375+digodk@users.noreply.github.com>
2023-08-23 13:55:23 -0300

    adicionando script de configuração do servidor de aplicações

--- init_scripts/apps_init.sh
+++ init_scripts/apps_init.sh
@@ -1,32 +1,16 @@
 #!/bin/bash
 # Atualiza o sistema
 echo "Atualizando o sistema"
-sudo apt update && sudo apt upgrade -y
-sudo apt-get install ca-certificates curl gnupg
+sudo dnf check-update && sudo dnf upgrade -y
 echo "Atualizado"
 echo "----------"
 
 # Instala o Docker
 echo "Instalando Docker"
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo dnf install docker -y
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 echo "Instalado Docker"
-echo "----------"
-
-# Instala o Certbot
-echo "Instalando Certbot"
-sudo apt install snapd -y
-sudo apt-get remove certbot -y
-sudo snap install --classic certbot -y
-sudo ln -s /snap/bin/certbot /usr/bin/certbot
-echo "Instalado Certbot"
 echo "----------"
 
 # Clona o projeto git
@@ -34,13 +18,10 @@ echo "Clonando o projeto git"
 git clone --filter=blob:none --no-checkout https://github.com/devs2blu-status200/projeto-lightsail.git
 cd projeto-lightsail
 git sparse-checkout init --cone
-git sparse-checkout set nginx
+git sparse-checkout set docker-compose.yml
 git checkout
 echo "Clonado"
 echo "----------"
 
 echo "Configuração inicial concluída, não esqueça de: "
-echo "-Obter os certificados usando o Certbot"
-echo "-Corrigir o /etc/resolv.conf conforme explicado no README"
-echo "-Ajustar os caminhos no docker-compose.yml do Nginx"
-echo "-Executar o docker-compose up -d para subir o servidor"
+echo "-Executar o docker-compose up -d para subir os aplicações"
