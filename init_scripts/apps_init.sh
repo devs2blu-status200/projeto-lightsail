84f59da6e4ab1a5f2eb0ee0a0566dfec4d1715ba
Diogo Klein <20893375+digodk@users.noreply.github.com>
2023-08-24 21:45:28 -0300

    melhorias para configuração de servidor de apps

--- init_scripts/apps_init.sh
+++ init_scripts/apps_init.sh
@@ -5,6 +5,30 @@ sudo dnf check-update && sudo dnf upgrade -y
 echo "Atualizado"
 echo "----------"
 
+# Adicionando usuário para github-actions
+read -p "Por favor, insira o nome do usuário que deseja criar: " USERNAME
+
+# 1. Criar o usuário
+sudo dnf install -y openssh-server
+sudo useradd $USERNAME
+
+# 2. Adicionar o usuário ao grupo wheel
+sudo usermod -aG wheel $USERNAME
+
+# 3. Criar um diretório .ssh no diretório inicial do usuário
+sudo mkdir /home/$USERNAME/.ssh
+sudo chmod 700 /home/$USERNAME/.ssh
+
+# 4. Solicitar a chave pública SSH do usuário e salvar no arquivo ~/.ssh/authorized_keys desse usuário
+echo "Por favor, cole sua chave pública SSH e pressione Ctrl+D quando terminar:"
+sudo tee /home/$USERNAME/.ssh/authorized_keys > /dev/null
+sudo chmod 600 /home/$USERNAME/.ssh/authorized_keys
+sudo chown -R $USERNAME:$USERNAME /home/$USERNAME/.ssh
+
+echo "Usuário $USERNAME criado e configurado para autenticação por chave SSH."
+echo "Não esqueça de configurar o a chave secreta no repositório para que as automações funcionem!"
+echo "----------"
+
 # Instala o Git
 echo "Instalando o Git"
 sudo dnf install git -y
@@ -22,6 +46,8 @@ echo "Instalando Docker"
 sudo dnf install docker -y
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo service docker start
+sudo usermod -aG docker $USER
 echo "Instalado Docker"
 echo "----------"
 
@@ -37,4 +63,4 @@ echo "Clonado"
 echo "----------"
 
 echo "Configuração inicial concluída, não esqueça de: "
-echo "-Executar o docker-compose up -d para subir os aplicações"
+echo "-Executar o docker-compose up -d para subir as aplicações"
