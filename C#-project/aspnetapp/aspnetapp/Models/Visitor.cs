b5d43683e4cc79a54110e27647bfab195ff2a76d
Lucas Theiss <lucasptheiss@gmail.com>
2023-08-29 18:58:40 -0300

    C# project

---
+++ C#-project/aspnetapp/aspnetapp/Models/Visitor.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VisitorApp.Models
+{
+    public class Visitor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime VisitDate { get; set; }
+    }
+}
---
+++ C#-project/aspnetapp/aspnetapp/Startup.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using SeuNameSpace;
+
+
+namespace MySqlDatabase
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string host = Environment.GetEnvironmentVariable("DB_HOST");
+            string database = Environment.GetEnvironmentVariable("DB_NAME");
+            string user = Environment.GetEnvironmentVariable("DB_USER");
+            string password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+            string connectionString = $"Server={host};Database={database};User={user};Password={password}";
+
+                services.AddDbContext<AppDbContext>(options =>
+                {
+                    options.UseMySQL(connectionString); // UseMySql instead of UseMySQL
+        });
+
+            // Other service configurations
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configuration for the request/response pipeline
+        }
+    }
+}
